be0fe09bcba5b424c12a3bae76eeb6abea981603
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-04-10 13:03:40 -0300

    feat: implementa o método estático createdRacesInstances em Dwarf

--- src/Races/Dwarf.ts
+++ src/Races/Dwarf.ts
@@ -2,12 +2,18 @@ import Race from './Race';
 
 export default class Dwarf extends Race {
   private _maxLifePoints: number;
+  private static _instances: number;
   constructor(name:string, dexterity: number) {
     super(name, dexterity);
     this._maxLifePoints = 80;
+    Dwarf._instances += 1;
   }
 
-  get maxLifePoints() {
+  public get maxLifePoints() {
     return this._maxLifePoints;
+  } 
+
+  public static createdRacesInstances() {
+    return Dwarf._instances;
   }
 }
