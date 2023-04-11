e44d91bd0a39a4f389a490028ecf980f083d85a9
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-04-11 11:00:29 -0300

    feat: cria classe Mage que extence Archetype e implementa métodos

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Mage extends Archetype {
+  private _energyType: EnergyType;
+  private static _archetypeInstances = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'mana';
+    Mage._archetypeInstances += 1;
+  }
+
+  public get energyType() {
+    return this._energyType;
+  }
+
+  public static createdArchetypeInstances() {
+    return Mage._archetypeInstances;
+  }
+}
