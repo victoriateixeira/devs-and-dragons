7b85463b4db5d8a1010527aa4bebd6e098ec9c8b
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-04-11 21:53:54 -0300

    fix: refatorando a classe Fighter e redefinindo o tipo de enemy para SimpleFighter

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -6,7 +6,7 @@ interface Fighter extends SimpleFighter {
   defense: number;
   energy?: Energy;
 
-  special?(enemy: Fighter): void;
+  special?(enemy: SimpleFighter): void;
   levelUp(): void;
 }
 
