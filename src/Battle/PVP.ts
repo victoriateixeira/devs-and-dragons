81cb416b066b3002148605ba2e901f9e31ba7ce6
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-04-12 09:28:02 -0300

    fix: corrige o parametro dos atributos player em PVP

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,10 +1,10 @@
-import Character from '../Character';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  private _player1: Character;
-  private _player2: Character;
-  constructor(player1: Character, player2: Character) {
+  private _player1: Fighter;
+  private _player2: Fighter;
+  constructor(player1: Fighter, player2: Fighter) {
     super(player1);
     super.fight();
     this._player1 = player1;
